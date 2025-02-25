4215951fd58c4630da79636dd8d3695db8cee00a
eyalyehiely <eyalwork0@gmail.com>
2025-02-25 23:18:53 +0200

    "Removed whitespace from end of file in emailRoutes.js"

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,6 +4,6 @@ const { sendEmailAndSaveUser } = require('../controllers/emailController');
 const router = express.Router();
 
 // Example routes
-router.post('/send-and-save', sendEmailAndSaveUser);
+router.post('/send-and-save', sendEmailAndSaveUser) ;
 
 module.exports = router;
